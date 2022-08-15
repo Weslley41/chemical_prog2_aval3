dd3b1f39dd787dd1ddb3b92c1b8c5de05a48b157
weslley41 <41weslley@gmail.com>
2022-08-15 12:00:51 -0300

    Starts implements Atom/Molecule classes

--- lib/atom.dart
+++ lib/atom.dart
@@ -1,1 +1,16 @@
-class Atom {}
+import 'package:chemical_prog3_aval/elements.dart';
+
+class Atom {
+  late String _symbol;
+
+  Atom({required String symbol}) {
+    if (Elements().hasElement(symbol)) {
+      _symbol = symbol;
+    } else {
+      Exception('Elemento inexistente "$symbol"');
+    }
+  }
+
+  @override
+  String toString() => _symbol;
+}
--- lib/molecule.dart
+++ lib/molecule.dart
@@ -1,1 +1,14 @@
-class Molecule {}
+class Molecule {
+  final String _formule;
+  final String _name;
+
+  Molecule({
+    required String formula,
+    required String name
+  }) :
+    _formule = formula,
+    _name = name;
+
+  String get formula => _formule;
+  int get weight => 0;
+}
