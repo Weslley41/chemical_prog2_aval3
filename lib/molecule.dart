5272cce6a51ff7b3c245f660a59975bf6c0751a5
weslley41 <41weslley@gmail.com>
2022-08-16 11:51:11 -0300

    implements Molecule

--- lib/molecule.dart
+++ lib/molecule.dart
@@ -1,14 +1,54 @@
-class Molecule {
-  final String _formule;
-  final String _name;
+import 'package:chemical_prog2_aval3/atom.dart';
 
-  Molecule({
-    required String formula,
-    required String name
-  }) :
-    _formule = formula,
+class Molecule implements Comparable<Molecule> {
+  late String _formula, _name;
+  final List<Map<String, dynamic>> _atoms = [];
+  int _weight = 0;
+
+  Molecule({required String formula, required String name}) {
+    this.formula = formula;
     _name = name;
+  }
 
-  String get formula => _formule;
-  int get weight => 0;
+  set formula(String formula) {
+    if (formula.isEmpty) throw Exception('Invalid formula: $formula');
+    _formula = formula;
+    final regex = RegExp('(?<atom>[A-Z][a-z]?d*)(?<count>[0-9]*)');
+    final atoms = regex.allMatches(formula);
+
+    for (var atom in atoms) {
+      String? atomSymbol = atom.namedGroup('atom');
+      int atomCount;
+
+      switch (atom.namedGroup('count')) {
+        case '':
+          atomCount = 1;
+          break;
+        case '1':
+          throw Exception('Invalid formula: $formula');
+        default:
+          atomCount = int.parse(atom.namedGroup('count')!);
+          break;
+      }
+
+      try {
+        Atom atom = Atom(symbol: atomSymbol!);
+        _atoms.add({
+          "atom": atom,
+          "count": atomCount
+        });
+        _weight += atom.weight * atomCount;
+      } catch (e) {
+        throw Exception('Invalid formula: $formula');
+      }
+    }
+  }
+
+  @override
+  String toString() => "$_name: $_formula";
+  String get formula => _formula;
+  int get weight => _weight;
+
+  @override
+  int compareTo(other) => (_weight - other.weight).truncate();
 }
